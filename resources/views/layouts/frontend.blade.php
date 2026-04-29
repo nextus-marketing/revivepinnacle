5e61702c8a517c30344ed4c8121de9998db62ff8
Sidh2003 <siddheshsonavane22@gmail.com>
2026-04-30 00:56:58 +0530

    d

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -51,10 +51,6 @@
 
 <body>
 
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MNSTVXNS" height="0" width="0"
-            style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
 
 
     <!-- back to top start -->
